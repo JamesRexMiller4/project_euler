7ab57839678f13cfd9e4a5f90cad66d2f0854358
James Miller <james.rex.miller.4@gmail.com>
2020-08-09 21:26:17 -0600

    Refactor, solution pending larger cases'

--- javascript_solutions/ex15.js
+++ javascript_solutions/ex15.js
@@ -5,80 +5,93 @@
 
 //  X X X
 //  X X X
-//  X X X = 24 routes 
+//  X X X = 20 routes 
 
 // How many such routes are there through a 20×20 grid?
 
+const twoByTwo = [
+  ["X", "X"],
+  ["X", "X"]
+]
+
 const threeByThree = [
-  ["X", "X", "X",],
-  ["X", "X", "X",],
-  ["X", "X", "X",]
+  ["X", "X", "X"],
+  ["X", "X", "X"],
+  ["X", "X", "X"]
 ]
 
 const fiveByFive = [
-  ["X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X",]
+  ["X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X"]
 ]
 
 const tenByTen = [
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
 ]
 
 const twentyByTwenty = [
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
-  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
 ]
 
 
 
 
-const calcLatticePathRoutes = (arr) => { 
-    let points = arr.length + 1;
-    let n = arr.length
+const calcLatticePathRoutes = (arr) => {
+    const route = "D".repeat(arr.length) + "R".repeat(arr.length);
 
-    const factorialLoop = (i) => {
-      let result = 1;
-      for (i ; i > 0; i--) {
-        result *= i
-      }
-      return result
+    const permutate = (str) => {
+      if (str.length <= 2) return str.length === 2 ? [str, [str[1] + str[0]]] : str
+      return str
+        .split('')
+        .reduce(
+          (acc, dir, i) => 
+          acc.concat(permutate(str.slice(0, i) + str.slice(i + 1)).map(val => dir + val)),
+          []
+        );
     }
 
-    let numRoutes = factorialLoop(n) * points
-    console.log(numRoutes)
-}
+    let allRoutes = permutate(route)
 
+    const removeDuplicates = (arr) => arr.filter((a, b) => arr.indexOf(a) === b);
+
+    let filteredAllRoutes = removeDuplicates(allRoutes);
+
+    // console.log("filteredRoutes", filteredAllRoutes);
+    console.log(filteredAllRoutes.length)
+}
+calcLatticePathRoutes(twoByTwo);
 calcLatticePathRoutes(threeByThree);
 calcLatticePathRoutes(fiveByFive);
-calcLatticePathRoutes(tenByTen);
-calcLatticePathRoutes(twentyByTwenty);
+// calcLatticePathRoutes(tenByTen);
+// calcLatticePathRoutes(twentyByTwenty);
