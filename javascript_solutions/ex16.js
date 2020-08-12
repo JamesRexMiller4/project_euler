da82b7444966e5497c3a7e91389ecf685817a9b6
James Miller <james.rex.miller.4@gmail.com>
2020-08-12 09:33:48 -0600

    Add solution to exercise 16

--- javascript_solutions/ex16.js
+++ javascript_solutions/ex16.js
@@ -3,21 +3,41 @@
 // What is the sum of the digits of the number 2^1000?
 
 const sumExponentialDigits = () => {
-  let sumArr = [];
   let resultSum = '';
   let carry = 0;
 
-  for (let i = 0; i < 1000; i+= 10) {
-    let sum = Math.pow(2, 10)
-    sumArr.push(sum)
-  }
+  const multiplyNumStringByTwo = (str) => {
+    let workingProduct = ''
+      for (let i = 0; i < str.length; i++) {
+        let char = str.charAt(str.length - 1 - i)
+  
+        let temp = (parseInt(char) * 2 + carry).toString();
+        let lastDigit = temp.charAt(temp.length - 1);
+        carry = parseInt(temp.slice(0, temp.length - 1));
+  
+        if (!carry) carry = 0;
+  
+        workingProduct = lastDigit + workingProduct;
+  
+      };
 
-  console.log(sumArr)
+      if (carry > 0) {
+        workingProduct = carry + workingProduct;
+        carry = 0;
+      }
 
-  let result = sumArr.reduce((acc, curVal) => acc *= curVal)
-  console.log(Math.pow(2, 1000))
-  console.log(result)
-  // console.log(resultSum);
-}
+    return workingProduct
+  };
+
+    for (let i = 0; i <= 1000; i++) {
+      if (i === 0) resultSum = "1";
+      if (i > 0) resultSum = multiplyNumStringByTwo(resultSum);
+    }
+  
+    let sumDigits = resultSum
+    .split('')
+    .reduce((acc, curVal) => acc += parseInt(curVal), 0);
+    console.log(sumDigits);
+  };
 
 sumExponentialDigits();
