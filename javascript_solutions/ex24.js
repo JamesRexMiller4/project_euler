00bc69903079880df757565975d82c187bea1481
James Miller <james.rex.miller.4@gmail.com>
2020-10-05 20:13:09 -0600

    Add permutation function

--- javascript_solutions/ex24.js
+++ javascript_solutions/ex24.js
@@ -8,5 +8,30 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 */
 
 const lexicographicPermutations = () => {
+  let nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8",
+  "9"];
+
+  const permutateString = (string) => {
+    if (string.length < 2) {
+      return string;
+    };
+
+    let permutationsArr = [];
+
+    for (let i = 0; i < string.length; i++) {
+      let char = string[i];
+
+      if (string.indexOf(char) != i) continue
+      
+      let remainingChars = string.slice(0, i) + string.slice(i + 1, string.length);
+
+      for (let permutation of permutateString(remainingChars)) {
+        permutationsArr.push(char + permutation);
+      }
+    }
+    return permutationsArr; 
+  };
+
+
 
 };
